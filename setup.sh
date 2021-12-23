5491a1c76aae6848fd8d4bbc73153fc82c01c581
johnpaulbin <johnpaulbin@gmail.com>
2021-12-23 05:09:19 +0000

    add setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+mkdir NudeNet
+
+wget "https://github.com/johnpaulbin/mega/releases/download/v1/classifier_lite.onnx" -O NudeNet/classifier_lite.onnx
+
+mv NudeNet .NudeNet
+
+echo setup complete
