98d0d3feb2b9a756ebfde5d8dcd54ab8be24f9e2
johnpaulbin <johnpaulbin@gmail.com>
2021-12-23 06:06:16 +0000

    debug

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -23,6 +23,7 @@ def nsfw_check(classifier, attachments):
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         urllib.request.install_opener(opener)
         urllib.request.urlretrieve(attachment.url, filename)
+        print(filename)
         file = classifier.classify(filename)
         os.remove(filename)
         return file[filename]['unsafe']
