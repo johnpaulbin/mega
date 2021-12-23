68f6fce3806b45b54b83ff9e067d3f5978923537
johnpaulbin <johnpaulbin@gmail.com>
2021-12-23 05:27:39 +0000

    tidy up setup.json

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget "https://github.com/johnpaulbin/mega/releases/download/v1/classifier_lite.onnx" -O ~/.NudeNet/classifier_lite.onnx
+wget "https://github.com/johnpaulbin/mega/releases/download/1.0/classifier_lite.onnx" -O ~/.NudeNet/classifier_lite.onnx
+
+wget "https://github.com/johnpaulbin/mega/releases/download/1.0/trust.json" -O trust.json
 
 echo setup complete
