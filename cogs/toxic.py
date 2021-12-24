710d69f392d66350f5d34bacfb3672bb1ff31a62
johnpaulbin <johnpaulbin@gmail.com>
2021-12-24 02:56:03 +0000

    update toxic command

---
+++ cogs/toxic.py
@@ -0,0 +1,43 @@
+from discord.ext import commands
+import discord
+from utils import parse_url, get_domain, get_logging_channel
+from discord.utils import get
+from discord.ext import commands
+from detoxify import Detoxify
+
+class Toxic(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.model = Detoxify('unbiased-small', device='cpu')
+
+
+    @commands.command(name='toxic')
+    async def toxicpredict(self, ctx, *, args):
+        await ctx.send(str(self.model.predict(args)))
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author != self.client.user:
+
+            #await message.channel.send(str(self.model.predict(message.content)))
+            print()
+            """
+            if get(message.guild.roles,
+                   id=836458265889079326) in message.author.roles:
+                return
+            # bypass link check if user has manage messages
+            if message.author.guild_permissions.manage_messages:
+                return
+            
+            
+            if 'bad' in self.pipeline.predict(
+                        parse_url(filtered_links)) and len(filtered_links) > 0:
+                    await message.delete()
+                    await get_logging_channel(message).send(
+                        f"⚠️ Phishing link(s) deleted: {message.author.mention} -> `{str(filtered_links)}` Context: ```{message.content}```"
+                    )
+            """
+            return
+
+def setup(client):
+    client.add_cog(Toxic(client))
--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+mkdir ~/.NudeNet/
+
 wget "https://github.com/johnpaulbin/mega/releases/download/1.0/classifier_lite.onnx" -O ~/.NudeNet/classifier_lite.onnx
 
 wget "https://github.com/johnpaulbin/mega/releases/download/1.0/trust.json" -O trust.json
+
+echo installing requirements...
+
+pip3 install -q -r requirements.txt
+pip3 uninstall dataclasses -y
 
 echo setup complete
