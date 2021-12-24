8d24cce823fcf2d7e1a0e463ed07399c2c9c9e0e
johnpaulbin <johnpaulbin@gmail.com>
2021-12-24 03:29:04 +0000

    final toxic (enable on message)

--- cogs/toxic.py
+++ cogs/toxic.py
@@ -5,11 +5,11 @@ from discord.utils import get
 from discord.ext import commands
 from detoxify import Detoxify
 
+
 class Toxic(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.model = Detoxify('unbiased-small', device='cpu')
-
 
     @commands.command(name='toxic')
     async def toxicpredict(self, ctx, *, args):
@@ -20,26 +20,18 @@ class Toxic(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author != self.client.user:
-
-            #await message.channel.send(str(self.model.predict(message.content)))
-            print()
-            """
-            if get(message.guild.roles,
-                   id=836458265889079326) in message.author.roles:
-                return
             # bypass link check if user has manage messages
             if message.author.guild_permissions.manage_messages:
                 return
-            
-            
-            if 'bad' in self.pipeline.predict(
-                        parse_url(filtered_links)) and len(filtered_links) > 0:
-                    await message.delete()
-                    await get_logging_channel(message).send(
-                        f"⚠️ Phishing link(s) deleted: {message.author.mention} -> `{str(filtered_links)}` Context: ```{message.content}```"
-                    )
-            """
+            prediction = sum(list(
+                self.model.predict(message.content).values()))
+            if 1.35 < prediction:
+                await message.delete()
+                await get_logging_channel(message).send(
+                    f"⚠️ Toxic message deleted from: {message.author.mention} Context: ```{message.content}``` with score `{prediction}`"
+                )
             return
+
 
 def setup(client):
     client.add_cog(Toxic(client))
