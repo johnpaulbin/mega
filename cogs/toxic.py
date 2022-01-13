53a893f6bf406ccebdc55b4f64fa4a7d189c6a51
johnpaulbin <johnpaulbin@gmail.com>
2022-01-13 17:58:42 +0000

    improve phishing detection

--- cogs/toxic.py
+++ cogs/toxic.py
@@ -10,7 +10,7 @@ def detoxify(model, message):
     prediction = list(model.predict(message).values())
     if prediction[0] > 0.95:
         return True, prediction[0], prediction[1]
-    elif float(format(prediction[1], '.4f')[4]) > 0.007: # there has got to be another way to format 5.123456e-5 better
+    elif float(format(prediction[1], '.4f')[4]) > 0.007: # there has got to be another way to format 1.234567e-8
         return True, prediction[0], prediction[1]
     return False, prediction[0], prediction[1]
 
@@ -28,7 +28,7 @@ class Toxic(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author != self.client.user:
-            # bypass link check if user has manage messages
+            # bypass toxic check if user can manage messages
             if message.author.guild_permissions.manage_messages:
                 return
             prediction, toxicity, severe = detoxify(self.model, message.content)
